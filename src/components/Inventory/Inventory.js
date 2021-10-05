747398a28cb74fc782d3d60ff5e5f461d730dac3
ixaman <63200084+ixaman@users.noreply.github.com>
2021-10-05 18:37:01 +0600

    changed remote url

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Inventory = () => {
     return (
         <div>
-            <h2>This is inventory</h2>
+            <h2>This is inventory page</h2>
         </div>
     );
 };
